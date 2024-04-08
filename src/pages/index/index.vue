e6f1548cec23daae5dbf3dcd59ed543a4e14eb60
乐天居易 <19836788252@163.com>
2024-04-08 21:06:47 +0800

    前台分类-组件封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,7 @@ import { getHomeBannerAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
 import type { BannerItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
 // 获取轮播图数据
 const bannelList = ref<BannerItem[]>([])
 const getHomeBannelData = async () => {
@@ -17,11 +18,17 @@ onLoad(() => {
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar></CustomNavbar>
+  <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannelList"></XtxSwiper>
+  <!-- 分类面板 -->
+  <CategoryPanel></CategoryPanel>
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
