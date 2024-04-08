0ac004f17c5ecb73dbbe83b92dfe5c874eb6c961
乐天居易 <19836788252@163.com>
2024-04-08 17:14:17 +0800

    轮播图-获取轮播图数据

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { getHomeBannerAPI } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
+const bannelList = ref([])
+const getHomeBannelData = async () => {
+  const res = await getHomeBannerAPI()
+  console.log(res)
+  bannelList.value = res.result
+}
+onLoad(() => {
+  getHomeBannelData()
+})
 </script>
 
 <template>
